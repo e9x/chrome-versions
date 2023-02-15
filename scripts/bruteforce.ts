e15ff0efe8be47294776e8d354df70cb267b424e
David Reed <reedswork@proton.me>
2023-02-14 19:32:02 -0500

    move all bruteforce methods into one script

--- scripts/bruteforce.ts
+++ scripts/bruteforce.ts
@@ -208,4 +208,36 @@ const bruteforce = async (board: string) => {
   console.log("Requests:", { initReq, realReq });
 };
 
-export default bruteforce;
+const getTargets = db.prepare("SELECT * FROM cros_target;");
+const getImages = db.prepare<[board: string]>(
+  "SELECT count(*) FROM cros_recovery_image WHERE board = ?;"
+);
+
+const [, , board] = process.argv;
+
+if (board) {
+  await bruteforce(board);
+} else {
+  console.warn("Bruteforcing every board...");
+
+  const targets = getTargets.all() as cros_target[];
+
+  for (const target of targets) {
+    const { "count(*)": imageCount } = getImages.get(target.board) as {
+      "count(*)": number;
+    };
+
+    if (imageCount !== 0) {
+      console.log(
+        target.board,
+        "already has",
+        imageCount,
+        "images scraped, skipping..."
+      );
+      continue;
+    }
+
+    console.log("Bruteforcing", target.board);
+    await bruteforce(target.board);
+  }
+}
