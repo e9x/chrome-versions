6fe8ddce51ef676fc6923d77a41c044d06877e18
David Reed <reedswork@proton.me>
2023-03-07 20:32:16 -0500

    builds

--- scripts/manualBuilds.ts
+++ scripts/manualBuilds.ts
@@ -8,6 +8,21 @@ const builds: cros_build[] = [
     chrome: "87.0.4280.109",
     platform: "13505.73.0",
   },
+  {
+    channel: "stable-channel",
+    chrome: "87.0.4280.109",
+    platform: "13099.102.0",
+  },
+  {
+    channel: "stable-channel",
+    chrome: "87.0.4280.109",
+    platform: "12239.67.0",
+  },
+  {
+    channel: "stable-channel",
+    chrome: "87.0.4280.109",
+    platform: "12371.75.1",
+  },
 ];
 
 console.log("Found", builds.length, "builds");
